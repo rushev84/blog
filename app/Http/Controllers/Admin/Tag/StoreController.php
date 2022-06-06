33ae9f48a333ca26d1ce60cd485efb1b5702816d
SergeyRushev <rushev84@yandex.ru>
2022-06-06 15:55:30 +0300

    create: base for CRUD for posts

--- app/Http/Controllers/Admin/Tag/StoreController.php
+++ app/Http/Controllers/Admin/Tag/StoreController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\Admin\Tag;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Tag\StoreRequest;
-use App\Models\Category;
 use App\Models\Tag;
-use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -7,6 +7,14 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
+                    <a href="{{ route('admin.post.index') }}" class="nav-link">
+                        <i class="nav-icon far fa-clipboard"></i>
+                        <p>
+                            Посты
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a href="{{ route('admin.category.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-th-list"></i>
                         <p>
