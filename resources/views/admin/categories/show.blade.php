48851bbdb50624f7394ebeea78def39edb93a0d9
SergeyRushev <rushev84@yandex.ru>
2022-06-06 11:35:40 +0300

    feat: view of a single category

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -7,7 +7,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Категории</h1>
+                    <h1 class="m-0">{{ $category->title }}</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -25,32 +25,18 @@
         <div class="container-fluid">
             <!-- Small boxes (Stat box) -->
             <div class="row">
-                <div class="col-1 mb-3">
-                    <a href="{{ route('admin.category.create')}}" class="btn btn-block btn-primary">Добавить</a>
-                </div>
-            </div>
-            <div class="row">
                 <div class="col-6">
                     <div class="card">
                         <div class="card-body table-responsive p-0">
                             <table class="table table-hover text-nowrap">
-                                <thead>
                                 <tr>
                                     <th>ID</th>
-                                    <th>Название категории</th>
-                                    <th>Действия</th>
+                                    <th>Название</th>
                                 </tr>
-                                </thead>
-                                <tbody>
-
-                                @foreach($categories as $category)
                                 <tr>
                                     <td>{{ $category->id }}</td>
                                     <td>{{ $category->title }}</td>
-                                    <td><a href="{{ route('admin.category.show', $category->id) }}"><i class="far fa-eye"></i></a></td>
                                 </tr>
-                                @endforeach
-                                </tbody>
                             </table>
                         </div>
                         <!-- /.card-body -->
