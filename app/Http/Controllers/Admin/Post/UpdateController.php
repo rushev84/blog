01cb8e1c31049ad351a48e2ab0bee6c8de2e1d85
SergeyRushev <rushev84@yandex.ru>
2022-06-07 14:36:07 +0300

    fix: bug with adding post where it was edited

--- app/Http/Controllers/Admin/Post/UpdateController.php
+++ app/Http/Controllers/Admin/Post/UpdateController.php
@@ -19,7 +19,6 @@ class UpdateController extends Controller
         $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
         $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
 
-        $post = Post::firstOrCreate($data);
         $post->update($data);
         $post->tags()->sync($tagIds);
 
