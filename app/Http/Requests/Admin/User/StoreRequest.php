b14fa82b8249aab1042aaa1f6ad8264ddfb53946
SergeyRushev <rushev84@yandex.ru>
2022-06-10 08:23:58 +0300

    feat: send password in email to registered user

--- app/Http/Requests/Admin/User/StoreRequest.php
+++ app/Http/Requests/Admin/User/StoreRequest.php
@@ -26,7 +26,6 @@ class StoreRequest extends FormRequest
         return [
             'name' => 'required|string',
             'email' => 'required|string|email|unique:users',
-            'password' => 'required|string',
             'role' => 'required|integer'
         ];
     }
@@ -40,8 +39,6 @@ class StoreRequest extends FormRequest
             'email.string' => 'Имя должно быть строкой',
             'email.email' => 'Ваша почта должна соответствовать формату email@some.domain',
             'email.unique' => 'Пользователь с таким email уже существует',
-            'password.required' => 'Это поле необходимо для заполнения',
-            'password.string' => 'Пароль должен быть строкой',
         ];
     }
 }
