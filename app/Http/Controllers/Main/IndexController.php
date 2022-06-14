ed5d39f8b762b416de6d51559894357d9f1f3c11
SergeyRushev <rushev84@yandex.ru>
2022-06-14 10:11:04 +0300

    ui: add boards to main admin page, add breadcrumbs to all chapters

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
