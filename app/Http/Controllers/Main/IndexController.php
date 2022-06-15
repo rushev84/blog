14b93e47e86f49147d36cd79fec9680ee5687388
SergeyRushev <rushev84@yandex.ru>
2022-06-15 11:28:20 +0300

    ui: main page for clients

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -13,6 +13,9 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('main.index');
+        $posts = Post::paginate(6);
+        $randomPosts = Post::get()->random(4);
+        $popularPosts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'DESC')->get()->take(4);
+        return view('main.index', compact('posts', 'randomPosts', 'popularPosts'));
     }
 }
